4da4592191e2f2a5cc95ffdb4747e3724b5bd82f
brsbgtr <bagaturbaris13@gmail.com>
2025-10-23 18:53:36 +0300

    Week5_part2

--- app/src/main/java/com/example/week5/MainActivity.java
+++ app/src/main/java/com/example/week5/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TableRow;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -48,6 +49,57 @@ public class MainActivity extends AppCompatActivity {
         return board [row][column] == 0;
 
     }
+    public int gameEnded(int row, int col){
+        int symbol = board[row][col];
+        boolean win = true;
+
+        for(int i=0; i<3; i++){
+            if(board[i][col] != symbol){
+                win = false;
+                break;
+            }
+        }
+        if(win){
+            return symbol;
+        }
+        //check rows +
+        //check diagonals +
+        win = true;
+        for (int j=0; j<3; j++){
+            if(board[row][j] != symbol){
+                win = false;
+                break;
+            }
+        }
+        if(win){
+            return symbol;
+        }
+        if(row == col){
+            win = true;
+            for(int i=0; i<3; i++){
+                if(board [i][i] != symbol){
+                    win = false;
+                    break;
+                }
+            }
+            if(win){
+                return symbol;
+            }
+        }
+        if(row + col == 2){
+            win = true;
+            for (int i=0; i<3; i++){
+                if(board [i][2-i] != symbol){
+                    win = false;
+                    break;
+                }
+            }
+            if(win){
+                return symbol;
+            }
+        }
+        return -1;
+    }
 
     class CellListener implements View.OnClickListener {
         int row, column;
@@ -72,6 +124,59 @@ public class MainActivity extends AppCompatActivity {
                 ((Button)v).setText(PLAYER_2);
                 board[row][column] =2;
             }
+            if(gameEnded(row,column) == -1){
+                player1_Turn =! player1_Turn;
+
+            }
+            else if (gameEnded(row,column) == 0){
+                Toast.makeText(MainActivity.this, "It is a DRAW", Toast.LENGTH_LONG).show();
+
+            }
+            else if (gameEnded(row,column) == 1){
+                Toast.makeText(MainActivity.this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Player 2 Wins", Toast.LENGTH_LONG).show();
+            }
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putBoolean("player1Turn", player1_Turn);
+        byte[] boardSingle = new byte [9];
+        for(int i =0; i<3; i++){
+            for(int j =0; j<3; j++){
+                boardSingle[3*i+j]=board[i][j];
+            }
+        }
+
+        outState.putByteArray("board", boardSingle);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        player1_Turn = savedInstanceState.getBoolean("player1Turn");
+        byte[] boardSingle = savedInstanceState.getByteArray("board");
+        for(int i=0; i<9; i++){
+            board[i/3][i%3] = boardSingle[i];
+        }
+        TableLayout table = findViewById(R.id.board);
+        for (int i=0; i<3; i++){
+            TableRow row = (TableRow)table.getChildAt(i);
+            for(int j=0; j<3; j++){
+                Button button = (Button) row.getChildAt(j);
+                if(board[i][j] == 1){
+                    button.setText("X");
+                }
+                else if (board[i][j] == 2){
+                    button.setText("0");
+                }
+
+            }
+        }
+
     }
 }
